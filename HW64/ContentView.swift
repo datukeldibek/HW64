34ae96b0a2035f8812ec19cd84d10380b09ba346
Datkayim <dkeldibekova08@gmail.com>
2023-04-13 17:23:56 +0600

    Initial Commit

---
+++ HW64/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW64
+//
+//  Created by Jarae on 13/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW64/HW64App.swift
@@ -0,0 +1,17 @@
+//
+//  HW64App.swift
+//  HW64
+//
+//  Created by Jarae on 13/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HW64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
